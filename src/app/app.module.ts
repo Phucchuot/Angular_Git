a055d2521f11dab942ea5bebfe63bb09a8a0a8c4
PHUC NGUYEN <nguyenngocphuc1712@gmail.com>
2022-04-07 14:58:31 +0700

    Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomesComponent } from './homes/homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule,
